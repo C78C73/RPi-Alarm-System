620c877e7a6b5b31ba51c1fe55813ab5a03f5cb8
Gage <35780576+C78C73@users.noreply.github.com>
2025-04-25 23:31:43 -0500

    created base files

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# RPI-Alarm-System
- This is a project to build a alarm system using rasperry pi's, sensors and python
+# RPi-Alarm-System
+### This is a project to build a alarm system using rasperry pi's, sensors, and Python
+
+```
+# Email sending when motion is detected by a sensor
+import smtplib
+from email.mime.text import MIMEText
+
+sender_email = "**INSERT EMAIL HERE**"
+app_password = "**INSERT GOOGLE APP PASSWORD**"
+
+email_recipient = "**INSERT EMAIL OF WHO YOU WANT TO SEND TO HERE**"
+
+msg = MIMEText("** Motion at door **")
+msg['From'] = sender_email
+msg['To'] = email_recipient
+msg['Subject'] = "🚨 Pi ALERT🚨"
+
+try:
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login(sender_email, app_password)
+    server.sendmail(sender_email, [email_recipient], msg.as_string())
+    server.quit()
+    print("Message sent to email.")
+except Exception as error:
+    print("Failed to send message:", error)
+```
---
+++ Template.py
@@ -0,0 +1,43 @@
+# Email sending when motion is detected by a sensor
+import smtplib
+from email.mime.text import MIMEText
+from time import sleep
+from datetime import datetime
+from gpiozero import MotionSensor
+
+def SendEmailAlert():
+    sender_email = ""
+    app_password = ""
+
+    email_recipient = ""
+
+    msg = MIMEText("** Motion at door **")
+    msg['From'] = sender_email
+    msg['To'] = email_recipient
+    msg['Subject'] = "🚨 Pi ALERT🚨"
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(sender_email, app_password)
+        server.sendmail(sender_email, [email_recipient], msg.as_string())
+        server.quit()
+        print("\nMessage sent to email.\n")
+    except Exception as error:
+        print("\nFailed to send message:", error, "\n")
+
+def MotionDetected():
+    pir = MotionSensor(4) # fix this number when you build it
+    
+    while True:
+        if (pir.motion_detected):
+            print("Motion detected @ " + str(datetime.now()))
+            sleep(1)
+            #return True
+        else:
+            sleep(1)
+
+def Main():
+    if (MotionDetected):
+        SendEmailAlert()
+
+Main()
